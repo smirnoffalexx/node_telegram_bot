2d46230b9f167c2cea0f0b78062b5f7c313a470f
smirnoffalexx <smirnov.am16@physics.msu.ru>
2022-01-27 03:08:14 +0300

    add webHook

--- README.md
+++ README.md
@@ -4,3 +4,17 @@ There is a telegram bot on node.js
 To run bot:
 
 npm start or node index
+
+
+To run local web server:
+
+snap install ngrok
+
+ngrok http 3000
+
+Then copy https URL and paste it to default.json
+
+
+To run debug mode:
+
+DEBUG=node-telegram-bot-api npm start 
--- index.js
+++ index.js
@@ -3,9 +3,31 @@
 
 const TelegramBot = require('node-telegram-bot-api')
 const config = require('config')
+const Koa = require('koa')
+const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
 
 const Token = config.get('token')
-const bot = new TelegramBot(Token,  { polling: true })
+const bot = new TelegramBot(Token)
+bot.setWebHook(`${config.get('url')}/bot`)
+
+const app = new Koa()
+
+const router = Router() // new Router()
+router.post('/bot', ctx => {
+    console.log(ctx)
+    const { body } = ctx.request
+    bot.processUpdate(body)
+    ctx.status = 200
+})
+
+app.use(bodyParser())
+app.use(router.routes())
+
+const port = config.get('port')
+app.listen(port, () => {
+    console.log(`Listening on ${port}`)
+})
 
 bot.on('message', msg => {
     const { chat: { id }} = msg
